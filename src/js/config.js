de60dd54eb854486be3dc2e23f4479abbdc75432
chenqingy <759484579@qq.com>
2017-09-26 19:52:57 +0800

    首页和详情页基本页面布局完成

--- src/js/config.js
+++ src/js/config.js
@@ -4,12 +4,14 @@ require.config({
     // 配置短路径（别名）
     paths:{
         jquery:'../lib/jquery-3.1.1',
-        xcarousel:'../lib/jquery-xCarousel/jquery.xcarousel'
+        xcarousel:'../lib/jquery-xCarousel/jquery.xcarousel',
+        jqueryui:'../lib/jquery-ui-1.12.1/jquery-ui'
     },
 
     // 配置依赖
     shim:{
         // xcarousel依赖jquery
-        xcarousel:['jquery']
+        xcarousel:['jquery'],
+        jqueryui:['jquery']
     }
 });
--- src/js/datalist.js
+++ src/js/datalist.js
@@ -19,7 +19,22 @@ require(['config'],function(){
         })
         // 以上是导航栏点击效果
         
-        
+        // 人气排行榜tab标签切换
+        var $ul_tabs = $('#tabs')
+        var $content = $ul_tabs.next().children();
+        var $liItems = $('#tabs').children();
+        // 隐藏除了第一个意外的内容
+        $content.slice(1).hide();
+        // 给第一个li加高亮
+        $liItems.first().addClass('active');
+        $ul_tabs.on('mouseenter','li',function(){
+            // 获取当前索引值
+            var idx = $(this).index();
+            // 高亮
+            $(this).addClass('active').siblings('li').removeClass();
+            // 显示对应的内容
+            $content.eq(idx).show().siblings('.tabcontent').hide();
+        })
         
         
         
